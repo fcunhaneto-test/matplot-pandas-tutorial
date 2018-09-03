72f890836beb31fd411bf1ebf574856723d2c375
Francisco Cunha Neto <fcunhaneto@gmail.com>
2018-09-03 12:15:12 -0300

    Pandas Matplot e textos

---
+++ pandas e matplot textos/matplot-pandas-1.py
@@ -0,0 +1,117 @@
+import pandas as pl
+import matplotlib.pyplot as plt
+
+"""
+Lendo um arquivo .csv com pandas
+"""
+dataframe = pl.read_csv('/home/francisco/Projects/Pycharm/'
+                        'matplot-pandas-tutorial/files/annual-real-gnp-us-1909-to-1970.csv')
+
+plt.figure(figsize=(8, 6))
+plt.plot(dataframe['Year'], dataframe['GNP'])
+plt.xlabel('Year')
+plt.ylabel('GNP')
+plt.title('PIB EUA Real Anual de 1909 a 1970')
+plt.savefig('annual-real-gnp-us-1909-to-1970.png')
+
+plt.close()
+
+"""
+Incluindo annotates
+"""
+plt.figure(figsize=(8, 6))
+plt.plot(dataframe['Year'], dataframe['GNP'])
+plt.xlabel('Year')
+plt.ylabel('GNP')
+plt.title('PIB EUA Real Anual de 1909 a 1970')
+
+# matplotlib.pyplot.annotate(*args, **kwargs)
+# s:str - The text of the annotation
+# xy:iterable - Length 2 sequence specifying the (x,y) point to annotate
+# xytext : iterable, optional - Length 2 sequence specifying the (x,y)
+#          to place the text at. If None, defaults to xy.
+# arrowprops: dict, optional
+plt.annotate('Fim da Segunda Gerra Mundial', xy=(1945, 355.2), color='red',
+             arrowprops=dict(arrowstyle='->'), xytext=(1920, 300))
+plt.annotate('Crash da Bolsa', xy=(1929, 203.6),
+             arrowprops=dict(arrowstyle='->'), xytext=(1909, 200))
+
+plt.savefig('gnp-us-1909-to-1970-annotate.png')
+
+plt.close()
+
+"""
+Annotates dentro de um box
+"""
+# plt.figure(figsize=(8, 6))
+# plt.plot(dataframe['Year'], dataframe['GNP'])
+# plt.xlabel('Year')
+# plt.ylabel('GNP')
+# plt.title('PIB EUA Real Anual de 1909 a 1970')
+
+# atributos para fonte:
+# color - cor da fonte
+# size - size in points or ['xx-small', 'x-small', 'small', 'medium', 'large',
+#          'x-large', 'xx-large']
+# weight - ['light' | 'ultralight' | 'normal' | 'bold' | 'heavy' | 'ultrabold'
+# style - ['normal' | 'italic' | 'oblique']
+# family - ['serif' | 'sans-serif' | 'cursive' | 'fantasy' | 'monospace']
+# plt.annotate(
+#     'Fim da Segunda Gerra Mundial',
+#     xy=(1945, 355.2),
+#     color='red',
+#     size='14',
+#     arrowprops=dict(arrowstyle='->'), xytext=(1920, 500),
+#     bbox = dict(boxstyle='round,pad=0.2', fc='yellow',
+#                 alpha=0.3),
+# )
+# plt.annotate(
+#     'Crash da Bolsa', xy=(1929, 203.6),
+#     arrowprops=dict(arrowstyle='->'),
+#     xytext=(1909, 200),
+#     color='yellow',
+#     weight='heavy',
+#     style='italic',
+#     bbox = dict(boxstyle='round,pad=0.2', fc='red',
+#                 alpha=1),
+# )
+#
+# plt.savefig('gnp-us-1909-to-1970-annotate-box.png')
+#
+# plt.close()
+
+"""
+Incluindo texto no gráfico
+"""
+font_1 = {'family':'serif', 'color':'#FFFC19', 'fontsize':'large' }
+font_2 = {'family':'fantasy', 'color':'yellow', 'size':'16'}
+font_3 = {'family':'serif', 'color':'yellow', 'size':'18', 'weight':'bold'}
+
+plt.figure(figsize=(8, 6))
+plt.plot(dataframe['Year'], dataframe['GNP'])
+plt.xlabel('Year')
+plt.ylabel('GNP')
+plt.title('Annual Real USA GNP 1909 to 1970')
+
+plt.annotate('Fim da Segunda Gerra Mundial', xy=(1945, 355.2),
+             arrowprops=dict(arrowstyle='->'), xytext=(1920, 300))
+plt.annotate('Crash da Bolsa', xy=(1929, 203.6),
+             arrowprops=dict(arrowstyle='->'), xytext=(1909, 200))
+
+# class matplotlib.text.Annotation(s, xy, xytext=None, xycoords='data',
+# textcoords=None, arrowprops=None, annotation_clip=None, **kwargs)
+plt.text(1909, 500, 'Pequeno texto de exemplo 1', fontdict=font_1,
+         backgroundcolor='#0971B2')
+
+plt.text(1909, 600, 'Pequeno texto de exemplo 2', fontdict=font_2,
+         backgroundcolor='#000000')
+
+# horizontalalignment or ha 	[ 'center' | 'right' | 'left' ]
+# verticalalignment or va 	[ 'center' | 'top' | 'bottom' | 'baseline' ]
+plt.text(1940, 700, 'Pequeno texto de exemplo 3', fontdict=font_3,
+         backgroundcolor='#000000', horizontalalignment='center',
+         verticalalignment='top')
+
+plt.savefig('gnp-us-1909-to-1970-text.png')
+
+plt.close()
--- retas plotagem simples/plotagem-simples.py
+++ retas plotagem simples/plotagem-simples.py
@@ -13,7 +13,7 @@ Plotagem de uma reta simples f(x) = 2x
 # plt.savefig('reta-simples.png')
 
 """
-Acrescentando Grid
+Acrescentando labels
 """
 # arr1 = np.array([1, 2, 3, 4, 5, 6, 7 ,8, 9, 10])
 # arr2 = np.array([2, 4, 6, 8, 10, 12, 14 ,16, 18, 20])
@@ -37,7 +37,7 @@ Acrescentando Grid
 # plt.ylabel('f(x) = 2x')
 # plt.grid(True)
 # plt.savefig('reta-simples-grid.png')
-
+#
 """
 Mais de uma linha no gráfico
 """
@@ -46,7 +46,7 @@ Mais de uma linha no gráfico
 #
 # plt.figure(figsize=(6, 4))
 # plt.plot(x, 2*x)
-# plt.plot(2*x, x/2)
+# plt.plot(x, x/2)
 # plt.xlabel('x')
 # plt.ylabel('f(x)')
 # plt.grid(True)
@@ -55,14 +55,51 @@ Mais de uma linha no gráfico
 """
 Escolhendo cores para as retas
 """
+# x = np.arange(1, 11)
+# print(x)
+#
+# plt.figure(figsize=(6, 4))
+# plt.plot(x, 2*x, color='#17a589') # green
+# plt.plot(x, x/2, color='red')
+# plt.plot(x, x+3, color='#f4d03f') # yellow
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.grid(True)
+# plt.savefig('reta-simples-cores.png')
+
+"""
+Inserindo titulo e legendas
+"""
+# x = np.arange(1, 11)
+# print(x)
+#
+# plt.figure(figsize=(6, 4))
+# plt.plot(x, 2*x, color='#17a589', label='f(x) = 2x') # green
+# plt.plot(x, x/2, color='red', label='f(x) = x/2')
+# plt.plot(x, x+3, color='#f4d03f', label='f(x) = x + 3') # yellow
+#
+# plt.grid(True)
+# plt.title('Retas Simples')
+# plt.legend()
+# plt.savefig('reta-titulo-legendas.png')
+# plt.close()
+
+"""
+Selecionando posição das legendas
+"""
 x = np.arange(1, 11)
 print(x)
 
 plt.figure(figsize=(6, 4))
 plt.plot(x, 2*x, color='#17a589') # green
-plt.plot(2*x, x/2, color='red')
+plt.plot(x, x/2, color='red')
 plt.plot(x, x+3, color='#f4d03f') # yellow
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
-plt.savefig('reta-simples-cores.png')
+plt.title('Retas Simples')
+plt.legend(['f(x) = 2x', 'f(x) = x/2', 'f(x) = x + 3'], loc=9, frameon=True,
+           framealpha=.5)
+plt.savefig('legendas-frameon-false.png')
+# plt.show()
+plt.close()
